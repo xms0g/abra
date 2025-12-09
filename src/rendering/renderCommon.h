19e4907147e0e0bdde7234182198cf3f8b54cb60
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-12-09 17:07:44 +0300

    refactor(rendering): redundant forward declaration of MaterialMap

--- src/rendering/renderCommon.h
+++ src/rendering/renderCommon.h
@@ -3,10 +3,8 @@
 #include <vector>
 #include <array>
 
-struct MaterialBatch;
 struct Texture;
 class Mesh;
-struct Material;
 class Shader;
 class Entity;
 
@@ -18,8 +16,6 @@ void setupMaterial(const Entity& entity, const Shader& shader);
 void drawMeshes(const std::vector<Mesh>& meshes);
 
 void drawQuad(uint32_t sceneTexture, uint32_t VAO);
-
-using MaterialMap = std::unordered_map<uint32_t, Material>;
 
 void bindTextures(const std::vector<Texture>& textures, const Shader& shader);
 
