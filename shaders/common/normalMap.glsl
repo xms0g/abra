01109d6cccd043c37ac95f2f4d1d4e2d8b485617
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-01-11 17:04:27 +0300

    refactor(shaders): minor

--- shaders/common/normalMap.glsl
+++ shaders/common/normalMap.glsl
@@ -2,7 +2,7 @@ vec3 normalMapping(mat3 TBN, vec2 texCoord, bool hasNormalMap) {
     if (!hasNormalMap)
         return TBN[2];
 
-    vec3 normal = texture(material.texture_normal, texCoord).rgb;
+    vec3 normal = texture(material.texture_normal, texCoord).xyz;
     // transform normal vector to range [-1,1]
     normal = normal * 2.0 - 1.0;
     // Transform N Tangent space to World space
