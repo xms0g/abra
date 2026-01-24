58d0e862b7bf27985060bc47cc769d574396643a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-01-25 00:56:53 +0300

    refactor(shaders): update PCF shadow calculation to use 5 samples instead of 3

--- shaders/common/blinnPhong.glsl
+++ shaders/common/blinnPhong.glsl
@@ -40,7 +40,7 @@ float calculateDirectionalShadow(vec4 fragPosLightSpace, vec3 normal, vec3 light
     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
     // PCF
     float shadow = 0.0;
-    int samples = 3;
+    int samples = 5;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
 
     for (int x = -samples; x <= samples; ++x) {
