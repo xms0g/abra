b0035d600960808a844b42693966df2da26e539c
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-04-18 02:50:27 +0300

    refactor(rendering): update `VertexLayout::push` methods to use designated initializers for improved readability and maintainability

--- src/rendering/buffers/vertexBuffer.h
+++ src/rendering/buffers/vertexBuffer.h
@@ -46,28 +46,28 @@ private:
 
 template<typename T>
 void VertexLayout::push(const uint32_t index, const int32_t size, const bool normalized) {
-	mAttributes.push_back(
-		{
-			std::is_same_v<T, float> ? GL_FLOAT : GL_INT,
-			index,
-			size,
-			normalized,
-			mStride
-		});
+	mAttributes.push_back({
+		.type = std::is_same_v<T, float> ? GL_FLOAT : GL_INT,
+		.index = index,
+		.size = size,
+		.normalized = normalized,
+		.offset = mStride,
+		.divisor = 0
+	});
 
 	mStride += size * sizeof(T);
 }
 
 template<typename T>
 void VertexLayout::pushVector(const uint32_t index, const bool normalized) {
-	mAttributes.push_back(
-		{
-			std::is_same_v<typename T::value_type, float> ? GL_FLOAT : GL_INT,
-			index,
-			T::length(),
-			normalized,
-			mStride
-		});
+	mAttributes.push_back({
+		.type = std::is_same_v<typename T::value_type, float> ? GL_FLOAT : GL_INT,
+		.index = index,
+		.size = T::length(),
+		.normalized = normalized,
+		.offset = mStride,
+		.divisor = 0
+	});
 
 	mStride += sizeof(T);
 }
@@ -80,15 +80,14 @@ void VertexLayout::pushMatrix(const uint32_t index, const uint32_t divisor) {
 	constexpr int componentsPerCol = ColumnType::length();
 
 	for (uint32_t i = 0; i < numCols; ++i) {
-		mAttributes.push_back(
-			{
-				std::is_same_v<typename T::value_type, float> ? GL_FLOAT : GL_INT,
-				index + i,
-				componentsPerCol,
-				false,
-				mStride,
-				divisor
-			});
+		mAttributes.push_back({
+			.type = std::is_same_v<typename T::value_type, float> ? GL_FLOAT : GL_INT,
+			.index = index + i,
+			.size = componentsPerCol,
+			.normalized = false,
+			.offset = mStride,
+			.divisor = divisor
+		});
 		mStride += sizeof(ColumnType);
 	}
 }
