4dc12e261c934b58f46e3920741c2cde958563bd
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-06-25 20:00:15 +0300

    refactor(io): add `loadFile` utility and include necessary headers in `filesystem.hpp`

--- src/io/filesystem.hpp
+++ src/io/filesystem.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <filesystem>
+#include <fstream>
+#include <sstream>
 #include <string>
 
 namespace fs {
@@ -7,5 +9,18 @@ namespace fs {
 inline std::string path(const std::string& p) {
     auto cwd = std::filesystem::current_path().parent_path();
     return cwd.append(p).string();
+}
+
+inline std::string loadFile(const std::string& p) {
+	std::ifstream file(path(p));
+
+	if (!file.is_open()) {
+		throw std::runtime_error(std::string("Failed to open shader file: ") + p);
+	}
+
+	std::stringstream ss;
+	ss << file.rdbuf();
+
+	return ss.str();
 }
 }
