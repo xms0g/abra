aaf5b334efd38e7a0bb12863f467ee9d42ab6a4a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-07-28 00:08:36 +0300

    refactor(rendering): replace `const char*` with `std::string` in `Shader` and add templated `setValue` methods for uniform handling

--- src/rendering/shader.h
+++ src/rendering/shader.h
@@ -8,7 +8,7 @@ struct ShaderResource {
 
 	ShaderResource() = default;
 
-	explicit ShaderResource(const char** code, const std::string& fn, uint32_t type);
+	ShaderResource(const std::string& code, const std::string& fn, uint32_t type);
 
 	ShaderResource(const ShaderResource& other) = delete;
 
@@ -28,6 +28,7 @@ struct ShaderResource {
 class Shader {
 public:
 	Shader() = default;
+
 	Shader(
 		const std::string& vs,
 		const std::string& fs,
@@ -51,38 +52,45 @@ public:
 	// use/activate the shader
 	void bind() const;
 
+	template<typename T>
+	void setValue(const std::string& name, const T& value) const;
+
+	template<typename T>
+	void setValue(const std::string& name, const T* value, uint32_t count);
+
 	// utility uniform functions
-	void setBool(const char* name, bool value) const;
+	void setBool(const std::string& name, bool value) const;
 
-	void setInt(const char* name, int32_t value) const;
+	void setInt(const std::string& name, int32_t value) const;
 
-	void setUint(const char* name, uint32_t value) const;
+	void setUint(const std::string& name, uint32_t value) const;
 
-	void setFloat(const char* name, float value) const;
+	void setFloat(const std::string& name, float value) const;
 
-	void setFloatArray(const char* name, const float* values, uint32_t count) const;
+	void setFloatArray(const std::string& name, const float* values, uint32_t count) const;
 
-	void setVec2(const char* name, const glm::vec2& value) const;
+	void setVec2(const std::string& name, const glm::vec2& value) const;
 
-	void setVec2(const char* name, float x, float y) const;
+	void setVec2(const std::string& name, float x, float y) const;
 
-	void setVec3(const char* name, const glm::vec3& value) const;
+	void setVec3(const std::string& name, const glm::vec3& value) const;
 
-	void setVec3(const char* name, float x, float y, float z) const;
+	void setVec3(const std::string& name, float x, float y, float z) const;
 
-	void setVec4(const char* name, const glm::vec4& value) const;
+	void setVec4(const std::string& name, const glm::vec4& value) const;
 
-	void setVec4(const char* name, float x, float y, float z, float w) const;
+	void setVec4(const std::string& name, float x, float y, float z, float w) const;
 
-	void setMat2(const char* name, const glm::mat2& mat) const;
+	void setMat2(const std::string& name, const glm::mat2& mat) const;
 
-	void setMat3(const char* name, const glm::mat3& mat) const;
+	void setMat3(const std::string& name, const glm::mat3& mat) const;
 
-	void setMat4(const char* name, const glm::mat4& mat) const;
+	void setMat4(const std::string& name, const glm::mat4& mat) const;
 
-	void setMat4Array(const char* name, const glm::mat4* matrices, size_t count) const;
+	void setMat4Array(const std::string& name, const glm::mat4* matrices, size_t count) const;
 
 	static std::string preprocess(const std::string& source);
+
 	static std::string preprocess(const std::string& source, std::unordered_set<std::string>& includedFiles);
 
 	static ShaderResource compileShader(const std::string& source, const std::string& fn, uint32_t type);
@@ -101,3 +109,37 @@ private:
 	// the program ID
 	uint32_t mID{};
 };
+
+template<typename T>
+void Shader::setValue(const std::string& name, const T& value) const {
+	if constexpr (std::is_same_v<T, bool>)
+		setBool(name, value);
+	else if constexpr (std::is_same_v<T, int32_t>)
+		setInt(name, value);
+	else if constexpr (std::is_same_v<T, uint32_t>)
+		setUint(name, value);
+	else if constexpr (std::is_same_v<T, float>)
+		setFloat(name, value);
+	else if constexpr (std::is_same_v<T, glm::vec2>)
+		setVec2(name, value);
+	else if constexpr (std::is_same_v<T, glm::vec3>)
+		setVec3(name, value);
+	else if constexpr (std::is_same_v<T, glm::vec4>)
+		setVec4(name, value);
+	else if constexpr (std::is_same_v<T, glm::mat2>)
+		setMat2(name, value);
+	else if constexpr (std::is_same_v<T, glm::mat3>)
+		setMat3(name, value);
+	else if constexpr (std::is_same_v<T, glm::mat4>)
+		setMat4(name, value);
+	else
+		static_assert(false, "Unsupported type");
+}
+
+template<typename T>
+void Shader::setValue(const std::string& name, const T* value, uint32_t count) {
+	if constexpr (std::is_same_v<T, float>)
+		setFloatArray(name, value, count);
+	else if constexpr (std::is_same_v<T, glm::mat4>)
+		setMat4Array(name, value, count);
+}
