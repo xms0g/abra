a575d51dda14fccd7e354c2fc7951ee6127e7819
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-08-12 03:07:14 +0300

    refactor(rendering, renderQueue): simplify empty and get methods, add string hash and equal for unordered map

--- src/rendering/context/renderQueue.hpp
+++ src/rendering/context/renderQueue.hpp
@@ -26,37 +26,67 @@ public:
 	QueueRegistry() = default;
 
 	[[nodiscard]]
-	bool empty(const std::string& queueName) const;
+	bool empty(std::string_view queueName) const;
 
 	template<typename T>
-	RenderQueue<T>& get(const std::string& queueName);
+	RenderQueue<T>& get(std::string_view queueName);
 
 	template<typename T>
-	void set(const std::string& queueName);
+	void set(std::string_view queueName);
 
 	template<typename T>
-	void emplace(const std::string& queueName, T& group);
+	void emplace(std::string_view queueName, T& group);
 
 private:
-	std::unordered_map<std::string, std::unique_ptr<IRenderQueue> > mQueue;
+	struct StringHash {
+		using is_transparent = void;
+
+		size_t operator()(const std::string_view value) const noexcept {
+			return std::hash<std::string_view>{}(value);
+		}
+
+		size_t operator()(const std::string& value) const noexcept {
+			return std::hash<std::string_view>{}(value);
+		}
+	};
+
+	struct StringEqual {
+		using is_transparent = void;
+
+		bool operator()(const std::string_view lhs, const std::string_view rhs) const noexcept {
+			return lhs == rhs;
+		}
+	};
+
+	std::unordered_map<std::string, std::unique_ptr<IRenderQueue>, StringHash, StringEqual > mQueue;
 };
 
-inline bool QueueRegistry::empty(const std::string& queueName) const {
-	return mQueue.at(queueName)->isEmpty();
+inline bool QueueRegistry::empty(const std::string_view queueName) const {
+	const auto it = mQueue.find(queueName);
+
+	if (it == mQueue.end())
+		return true;
+
+	return it->second->isEmpty();
 }
 
 template<typename T>
-RenderQueue<T>& QueueRegistry::get(const std::string& queueName) {
-	return static_cast<RenderQueue<T>&>(*mQueue.at(queueName));
+RenderQueue<T>& QueueRegistry::get(const std::string_view queueName) {
+	const auto it = mQueue.find(queueName);
+
+	if (it == mQueue.end())
+		throw std::out_of_range("Render queue not found");
+
+	return static_cast<RenderQueue<T>&>(*it->second);
 }
 
 template<typename T>
-void QueueRegistry::set(const std::string& queueName) {
+void QueueRegistry::set(const std::string_view queueName) {
 	mQueue.emplace(queueName, std::make_unique<RenderQueue<T> >());
 }
 
 template<typename T>
-void QueueRegistry::emplace(const std::string& queueName, T& group) {
+void QueueRegistry::emplace(const std::string_view queueName, T& group) {
 	try {
 		get<T>(queueName).push_back(group);
 	} catch (std::out_of_range&) {
